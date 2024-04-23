e4225312300f54cdeb42d9946a400544114b2829
eliran650 <eliranovadia7@gmail.com>
2024-04-23 20:12:09 +0300

    building...

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-
-
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -9,6 +9,30 @@ app = FastAPI()
 def index():
     return {'data': {'name': 'Eliran'}}
 
-@app.get('/about')
-def about():
-    return {'data': {'about page'}}
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'unpublished blogs'}
+
+
+@app.get('/blog/')
+def index(limit: int = 10, published: bool = True, sort: Optional[str] = None):
+    if published:
+        return {'data': f'{limit} published blogs from the db'}
+    else:
+        return {'data': f'{limit} blogs from the db'}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id, limit: int=10):
+    return {'data': {id,'1','2'}}
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+@app.post('/blog')
+def create_blog(request: Blog):
+    return {'data': f"blog is created with title as {request.title}"}
