4387be745c762e8ed90516abc74f72d40dfbb993
eliran650 <eliranovadia7@gmail.com>
2024-04-28 00:04:25 +0300

    JWT_TOKEN_AUTH

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -15,18 +15,18 @@ def all(db: Session = check_db, current_user: schemas.User = check_auth):
     return blog.get_all(db)
 
 @router.get('/{id}', status_code = 200, response_model = schemas.ShowBlog)
-def show(id, db: Session = check_db):
+def show(id, db: Session = check_db, current_user: schemas.User = check_auth):
     return blog.show(id, db)
 
 @router.post('/', status_code=status.HTTP_201_CREATED)
-def create(request: schemas.Blog, db: Session = check_db):
+def create(request: schemas.Blog, db: Session = check_db, current_user: schemas.User = check_auth):
     return blog.create(request, db)
 
 @router.put('/{id}', status_code = status.HTTP_202_ACCEPTED)
-def update(id,request: schemas.Blog, db: Session = check_db):
+def update(id,request: schemas.Blog, db: Session = check_db, current_user: schemas.User = check_auth):
     return blog.update(id, db, request)
 
 
 @router.delete('/{id}',status_code = status.HTTP_204_NO_CONTENT)
-def destroy(id, db: Session = check_db):
+def destroy(id, db: Session = check_db, current_user: schemas.User = check_auth):
     return blog.destroy(id, db)
