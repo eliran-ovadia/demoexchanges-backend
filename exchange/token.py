eaf408af2ba8d1ddf66ea40970f913406f2fb64d
eliranovadia7@gmail.com <eliranovadia7@gmail.com>
2024-05-06 15:39:52 +0300

    new_token

--- exchange/token.py
+++ exchange/token.py
@@ -2,9 +2,9 @@ from datetime import datetime, timedelta, timezone
 from jose import JWTError, jwt
 from . import schemas
 
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+SECRET_KEY = "b1f8e2ef0fb1874b24d54c8675ca002791f7769c0337725389c67c0d33f2c317"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_MINUTES = 1
 
 
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
