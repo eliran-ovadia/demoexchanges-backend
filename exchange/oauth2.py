7209e257f61230faac3dbd2818f582259aa97a28
eliranovadia7@gmail.com <eliranovadia7@gmail.com>
2024-06-19 21:16:35 +0300

    ...

--- exchange/oauth2.py
+++ exchange/oauth2.py
@@ -4,13 +4,11 @@ from . import token
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
-class RedirectException(HTTPException):
-    def __init__(self, redirect_url: str):
-        super().__init__(status_code=307, detail="Redirecting to login page")
-        self.redirect_url = redirect_url
-        
-        
 
 def get_current_user(data: str = Depends(oauth2_scheme)):
-    credentials_exception = RedirectException(redirect_url="/") #if JWT bearer not found
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
     return token.verify_token(data, credentials_exception)
