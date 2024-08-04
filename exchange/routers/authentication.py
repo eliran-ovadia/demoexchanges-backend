a676928fc6a66de948a79527c4982ff32ff39f60
eliran650 <eliranovadia7@gmail.com>
2024-08-04 17:50:49 +0300

    sleep_20_sex_token

--- exchange/routers/authentication.py
+++ exchange/routers/authentication.py
@@ -3,12 +3,14 @@ from fastapi.security import OAuth2PasswordRequestForm
 from .. import schemas, database, models, token
 from sqlalchemy.orm import Session
 from ..hashing import Hash
+from time import sleep
 
 router = APIRouter(tags = ['Authentication'])
 check_db = Depends(database.get_db)
 
 @router.post('/token')
 def get_token(request: OAuth2PasswordRequestForm = Depends(), db: Session = check_db):
+    sleep(20)
     #---------finding_the_user_in_the_database------------------
     user = db.query(models.User).filter(models.User.email == request.username).first()
     if not user:
