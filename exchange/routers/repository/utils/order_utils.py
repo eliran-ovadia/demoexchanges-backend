e2ab4a858459c321c32edd13174774bcc15b1fc8
eliranovadia7@gmail.com <eliranovadia7@gmail.com>
2024-11-24 19:28:32 +0200

    V1.0.5 - Continuing to optimizing the /order endpoint for a faster user experience.

--- exchange/routers/repository/utils/order_utils.py
+++ exchange/routers/repository/utils/order_utils.py
@@ -8,6 +8,7 @@ from exchange.app_logger import logger
 from exchange.routers.repository.utils.utils import *
 
 
+##################### Sell logic #####################
 def sell_handler(request: schemas.Order, db: Session, current_user: schemas.TokenData, symbol: str, price: float,
                  value: float):
     total_owned_stock = db.query(func.sum(models.Portfolio.amount)).filter(
@@ -20,14 +21,25 @@ def sell_handler(request: schemas.Order, db: Session, current_user: schemas.Toke
                             detail=f"Short selling is not supported at the moment,"
                                    f"you can sell a maximum of {total_owned_stock} stocks""")
 
+    total_profit = sell(current_user, db, price, request, symbol, value)
+
+    return schemas.AfterOrder(
+        symbol=symbol,
+        price=round(price, 2),
+        amount=request.amount,
+        type=request.type,
+        value=round(value, 2),
+        profit=round(total_profit, 2)
+    )
+
+
+def sell(current_user: schemas.TokenData, db: Session, price: float, request: schemas.Order, symbol: str, value: float):
     portfolio_entries = db.query(models.Portfolio).filter(
         models.Portfolio.user_id == current_user.id,
         models.Portfolio.symbol == symbol
     ).order_by(models.Portfolio.time_stamp.asc()).yield_per(100)
-
     remaining_amount_to_sell = request.amount
     total_profit = 0.0
-
     for entry in portfolio_entries:
         if remaining_amount_to_sell <= 0:
             break
@@ -40,11 +52,9 @@ def sell_handler(request: schemas.Order, db: Session, current_user: schemas.Toke
             total_profit += (price - entry.price) * remaining_amount_to_sell
             entry.amount -= remaining_amount_to_sell  # Update entry amount
             remaining_amount_to_sell = 0
-
     db.query(models.User).filter(models.User.id == current_user.id).update(
         {models.User.cash: models.User.cash + value}
     )
-
     transaction_time = datetime.now()
     db.add(
         models.History(
@@ -58,45 +68,59 @@ def sell_handler(request: schemas.Order, db: Session, current_user: schemas.Toke
             user_id=current_user.id
         )
     )
-    db.commit()  # Atomic action
+    try:
+        db.commit()  # Atomic action
+    except Exception as e:
+        db.rollback()
+        logger.error(f"Database commit failed when trying to sell: {e}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="An error occurred while processing the transaction"
+        )
+    return total_profit
 
-    return schemas.AfterOrder(
-        symbol=symbol,
-        price=round(price, 2),
-        amount=request.amount,
-        type=request.type,
-        value=round(value, 2),
-        profit=round(total_profit, 2)
-    )
 
-
+##################### Buy logic #####################
 def buy_handler(request: schemas.Order, db: Session, current_user: schemas.TokenData, symbol: str, price: float,
                 value: float):
-    user = find_user(db, current_user.id, )
-
-    if user.cash < value:
+    affected_rows = (
+        db.query(models.User)
+        .filter(models.User.id == current_user.id, models.User.cash >= value)
+        .update({models.User.cash: models.User.cash - value})
+    )
+    if affected_rows == 0:
         logger.error(f"User {current_user.email} attempted to buy with insufficient cash.")
-        raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail="Insufficient cash for buying")
+        raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail=f"Insufficient cash for buying {symbol}")
 
-    user.cash -= value
     # Insert portfolio and history rows
     transaction_time = datetime.now()
     new_portfolio_entry = models.Portfolio(symbol=symbol, amount=request.amount, time_stamp=transaction_time,
                                            price=price,
-                                           user_id=user.id
+                                           user_id=current_user.id
                                            )
-    db.add(new_portfolio_entry)
 
     transaction_history = models.History(symbol=symbol, price=price, amount=request.amount, type=request.type,
                                          value=value,
                                          profit=0.0,  # No profit for buy order
                                          time_stamp=transaction_time,
-                                         user_id=user.id
+                                         user_id=current_user.id
                                          )
-    db.add(transaction_history)
-    db.commit()
 
-    return schemas.AfterOrder(symbol=symbol, price=round(price, 2), amount=request.amount, type=request.type,
+    db.add_all([new_portfolio_entry, transaction_history])
+    try:
+        db.commit()  # Atomic action
+    except Exception as e:
+        db.rollback()
+        logger.error(f"Database commit failed when trying to buy: {e}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="An error occurred while processing the transaction"
+        )
+
+    return schemas.AfterOrder(symbol=symbol,
+                              price=round(price, 2),
+                              amount=request.amount,
+                              type=request.type,
                               value=round(value, 2),
                               profit=0.0
                               )
--- exchange/schemas.py
+++ exchange/schemas.py
@@ -1,7 +1,7 @@
 import re
 from datetime import datetime
 
-from pydantic import BaseModel, EmailStr, field_validator, constr, confloat, PositiveInt, Field
+from pydantic import BaseModel, EmailStr, field_validator, constr, confloat, PositiveInt, Field, ValidationError
 
 
 class Pagination(BaseModel):
@@ -15,8 +15,8 @@ class Stock(BaseModel):
     @field_validator("symbol")
     def uppercase_symbol_and_length(cls, v):
         v = v.upper()  # Convert to uppercase
-        if not (1 <= len(v) <= 4 and v.isalpha()):
-            raise ValueError("Symbol must be between 1 and 4 alphabetic characters")
+        if not (1 <= len(v) <= 5 and v.isalpha()):
+            raise ValueError("Symbol must be between 1 and 5 alphabetic characters")
         return v
 
 
@@ -90,7 +90,7 @@ class User(BaseModel):
 
 
 class Order(BaseModel):
-    symbol: str  # convert to Stock schema?
+    symbol: str
     amount: PositiveInt
     type: str
 
@@ -99,12 +99,6 @@ class Order(BaseModel):
         if v.lower() not in ['buy', 'sell']:
             raise ValueError("Type must be either 'buy' or 'sell'")
         return v.capitalize()
-
-    @field_validator('amount')
-    def validate_amount(cls, v):
-        if v > 10_000:
-            raise ValueError("Cannot buy more than 10,000 stocks a once")
-        return v
 
 
 class Login(BaseModel):
