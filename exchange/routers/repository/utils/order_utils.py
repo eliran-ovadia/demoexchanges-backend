685dd9ba4e523b7769e7f19a28db69b5431ce9b3
eliran650 <eliranovadia7@gmail.com>
2024-11-24 18:19:19 +0200

    1.0.4 - order endpoint optimization

--- exchange/routers/repository/utils/order_utils.py
+++ exchange/routers/repository/utils/order_utils.py
@@ -10,22 +10,20 @@ from exchange.routers.repository.utils.utils import *
 
 def sell_handler(request: schemas.Order, db: Session, current_user: schemas.TokenData, symbol: str, price: float,
                  value: float):
-    user = find_user(db, current_user.id)
-
     total_owned_stock = db.query(func.sum(models.Portfolio.amount)).filter(
-        models.Portfolio.user_id == user.id,
+        models.Portfolio.user_id == current_user.id,
         models.Portfolio.symbol == symbol
     ).scalar() or 0
 
     if total_owned_stock < request.amount:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-                            detail=f"""Short selling is not supported at the moment,
-                                        you can sell a maximum of {total_owned_stock} stocks""")
+                            detail=f"Short selling is not supported at the moment,"
+                                   f"you can sell a maximum of {total_owned_stock} stocks""")
 
     portfolio_entries = db.query(models.Portfolio).filter(
-        models.Portfolio.user_id == user.id,
+        models.Portfolio.user_id == current_user.id,
         models.Portfolio.symbol == symbol
-    ).order_by(models.Portfolio.time_stamp.asc()).all()
+    ).order_by(models.Portfolio.time_stamp.asc()).yield_per(100)
 
     remaining_amount_to_sell = request.amount
     total_profit = 0.0
@@ -37,27 +35,30 @@ def sell_handler(request: schemas.Order, db: Session, current_user: schemas.Toke
         if entry.amount <= remaining_amount_to_sell:
             total_profit += (price - entry.price) * entry.amount
             remaining_amount_to_sell -= entry.amount
-            db.delete(entry)  # delete entire row
+            db.delete(entry)  # Delete entire row
         else:
             total_profit += (price - entry.price) * remaining_amount_to_sell
-            entry.amount -= remaining_amount_to_sell  # delete part of the row
+            entry.amount -= remaining_amount_to_sell  # Update entry amount
             remaining_amount_to_sell = 0
 
-    user.cash += value
+    db.query(models.User).filter(models.User.id == current_user.id).update(
+        {models.User.cash: models.User.cash + value}
+    )
 
     transaction_time = datetime.now()
-    transaction_history = models.History(
-        symbol=symbol,
-        price=price,
-        amount=request.amount,
-        type=request.type,
-        value=value,
-        profit=total_profit,
-        time_stamp=transaction_time,
-        user_id=user.id
+    db.add(
+        models.History(
+            symbol=symbol,
+            price=price,
+            amount=request.amount,
+            type=request.type,
+            value=value,
+            profit=total_profit,
+            time_stamp=transaction_time,
+            user_id=current_user.id
+        )
     )
-    db.add(transaction_history)
-    db.commit()
+    db.commit()  # Atomic action
 
     return schemas.AfterOrder(
         symbol=symbol,
