2faa73eefb61c6d065714036fc1513eeb9cf759d
eliranovadia7@gmail.com <eliranovadia7@gmail.com>
2024-11-30 22:28:43 +0200

    V1.0.6 - WIP - In the middle of optimizing the /parsedQuote endpoint.
    implemented a TTL based cache to QuoteHandler class.
    
    Needs to work on get_parsed_quote function.

--- src/exchange/external_client_handlers/client_response_models/quote_handler.py
+++ src/exchange/external_client_handlers/client_response_models/quote_handler.py
@@ -1,12 +1,17 @@
+from cachetools import TTLCache
+from typing import Optional
 from fastapi import HTTPException, status
-
 from src.exchange.app_logger import logger
 
 
-class QuoteHandler:
-    def __init__(self, symbol=None, name=None, exchange=None, currency=None, open=None, high=None, low=None, close=None,
-                 volume=None, change=None, percent_change=None, average_volume=None, fifty_two_week=None, **kwargs):
+quote_cache = TTLCache(maxsize=1000, ttl=20)
 
+class QuoteHandler:
+    def __init__(self, symbol: Optional[str] = None, name: Optional[str] = None, exchange: Optional[str] = None,
+        currency: Optional[str] = None, open: Optional[float] = None, high: Optional[float] = None,
+        low: Optional[float] = None, close: Optional[float] = None, volume: Optional[int] = None,
+        change: Optional[float] = None, percent_change: Optional[float] = None, average_volume: Optional[int] = None,
+        fifty_two_week: Optional[dict] = None, **kwargs):
         self.symbol = symbol
         self.name = name
         self.exchange = exchange
@@ -20,8 +25,8 @@ class QuoteHandler:
         self.percent_change = percent_change
         self.average_volume = average_volume
         self.fifty_two_week = fifty_two_week
-        self.fifty_two_week_high = fifty_two_week.get('high', None)
-        self.fifty_two_week_low = fifty_two_week.get('low', None)
+        self.fifty_two_week_high = fifty_two_week.get("high") if fifty_two_week else None
+        self.fifty_two_week_low = fifty_two_week.get("low") if fifty_two_week else None
 
     def to_parsed_quote(self) -> dict:
         try:
@@ -30,24 +35,26 @@ class QuoteHandler:
                 "full_name": self.name,
                 "exchange": self.exchange,
                 "currency": self.currency,
-                "open": round(float(self.open), 2),
-                "high": round(float(self.high), 2),
-                "low": round(float(self.low), 2),
-                "close": round(float(self.close), 2),
-                "volume": int(self.volume),
-                "change": round(float(self.change), 2),
-                "percent_change": round(float(self.percent_change), 2),
-                "avg_volume": int(self.average_volume),
-                "year_range_high": round(float(self.fifty_two_week.get("high", 0)), 2) if self.fifty_two_week.get(
-                    "high") else None,
-                "year_range_low": round(float(self.fifty_two_week.get("low", 0)), 2) if self.fifty_two_week.get(
-                    "low") else None,
+                "open": round(float(self.open), 2) if self.open else None,
+                "high": round(float(self.high), 2) if self.high else None,
+                "low": round(float(self.low), 2) if self.low else None,
+                "close": round(float(self.close), 2) if self.close else None,
+                "volume": int(self.volume) if self.volume else None,
+                "change": round(float(self.change), 2) if self.change else None,
+                "percent_change": round(float(self.percent_change), 2) if self.percent_change else None,
+                "avg_volume": int(self.average_volume) if self.average_volume else None,
+                "year_range_high": round(float(self.fifty_two_week_high), 2)
+                if self.fifty_two_week_high
+                else None,
+                "year_range_low": round(float(self.fifty_two_week_low), 2)
+                if self.fifty_two_week_low
+                else None,
             }
 
-            # Logging for Debug - log missing keys
+            # Log missing keys
             missing_keys = [key for key, value in parsed_quote.items() if value is None]
             if missing_keys:
-                logger.critical(f"Missing keys in quote for {self.symbol}: {missing_keys} - from QuoteParser")
+                logger.warning(f"Missing keys in quote for {self.symbol}: {missing_keys}")
 
             return parsed_quote
 
@@ -55,5 +62,16 @@ class QuoteHandler:
             logger.critical(f"Error parsing quote for {self.symbol}: {e}")
             raise HTTPException(
                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                detail=f"Error parsing quote data for symbol {self.symbol}"
+                detail=f"Error parsing quote data for symbol {self.symbol}",
             )
+
+
+def get_cached_quote_handler(symbol: str, **kwargs) -> QuoteHandler:
+    if symbol in quote_cache: # Cache hit
+        return quote_cache[symbol]
+
+    # Cache miss
+    fetched_data = {"symbol": symbol, **kwargs}
+    handler = QuoteHandler(**fetched_data)
+    quote_cache[symbol] = handler
+    return handler
